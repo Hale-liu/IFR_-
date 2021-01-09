0c0b301470c74c7b20af6f783506c668db099a6d
Hale-liu <2293708026@qq.com>
2021-01-09 19:27:06 +0800

    1/9 重新加回了看门狗，加入了状态机（正常模式，顺时针自转，逆时针自转）

---
+++ Mecanum Chassis Control/MDK-ARM/RTE/_Mecanum_Chassis_Control/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Mecanum Chassis Control' 
+ * Target:  'Mecanum Chassis Control' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
